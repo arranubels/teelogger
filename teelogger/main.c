87095d42dd11fe6c6386cfece8f04ae517e7e5b9
aubels <devsde4>
2013-01-25 15:48:30 +1100

    Initial Commit

---
+++ teelogger/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  teelogger
+//
+//  Created by Arran Ubels on 25/01/13.
+//  Copyright (c) 2013 Arran Ubels. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
